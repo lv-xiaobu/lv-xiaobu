cb8b15406b2f233870bfb259a884498069bb4ae5
lv-xiaobu <lhg_523@163.com>
2024-01-24 18:09:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ const readme = `\
 ⏰ Updated on ${new Date().toUTCString()}
 
 ---
+### My GitHub Contributions
+
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/AxyLm/axylm/output/github-contribution-grid-snake-dark.svg">
+  <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/AxyLm/axylm/output/github-contribution-grid-snake.svg">
+  <img alt="github contribution grid snake animation" src="https://raw.githubusercontent.com/AxyLm/axylm/output/github-contribution-grid-snake.svg">
+</picture>
 \
 `
 
